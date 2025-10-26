093b001e955265c3a5fa9830e1dd9fe50504714d
Nikolai Gavriushin <gna@pm.me>
2025-10-26 21:35:44 +0400

    connect fonts

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,11 +6,11 @@ import Footer from "../Footer/Footer";
 function App() {
 
   return (
-    <>
+    <div className="page">
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   )
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,11 @@
 import "./Main.css";
 
+import mainImage from "../../assets/main.png";
+
 function Main() {
   return(
     <main>
+      <img src={mainImage} alt="Main image" />
       <p>React app for real-time USD exchange rates</p>
     </main>
   )
